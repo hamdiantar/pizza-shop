9c9487fdd6bda878642009237c879efba0ce2a3a
hamdiantar <hamdi@webzzmaster.com>
2020-03-12 21:15:28 +0200

    add namespace in api.php

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,19 @@
 <?php
 
-Route::prefix('pizzas')->group(function(){
-    Route::post('create', 'PizzasController@create');
-    Route::put('update/{pizza}', 'PizzasController@update');
-    Route::delete('delete/{pizza}', 'PizzasController@delete');
-});
+Route::namespace('Api')->group(function () {
+    Route::prefix('pizzas')->group(function () {
+        Route::post('create', 'PizzasController@create');
+        Route::put('update/{pizza}', 'PizzasController@update');
+        Route::delete('delete/{pizza}', 'PizzasController@delete');
+    });
 
-Route::post('customers/create', 'CustomersController@create');
-Route::prefix('orders')->group(function(){
-    Route::post('create', 'OrdersController@create');
-    Route::patch('update-status/{order}', 'OrdersController@updateStatus');
-    Route::delete('delete/{order}', 'OrdersController@deleteOrder');
-    Route::get('filter', 'OrdersController@getOrder');
-    Route::get('list', 'OrdersController@listOrders');
+    Route::post('customers/create', 'CustomersController@create');
+
+    Route::prefix('orders')->group(function () {
+        Route::post('create', 'OrdersController@create');
+        Route::patch('update-status/{order}', 'OrdersController@updateStatus');
+        Route::delete('delete/{order}', 'OrdersController@deleteOrder');
+        Route::get('filter', 'OrdersController@getOrder');
+        Route::get('list', 'OrdersController@listOrders');
+    });
 });
